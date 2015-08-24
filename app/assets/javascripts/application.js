9aae40ff1e395200784fc35d87184f9ba1e92cb2
Walter <walter.augustine10@gmail.com>
2015-08-24 17:05:34 -0400

    added front end models for question and answer. added front end views for question and answer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,85 @@
 $(document).ready(function () {
+  Question.fetch().then(function (questions) {
+    questions.forEach(function (question) {
+      var view = new QuestionView(question)
+      view.render();
+    })
+  })
 
 
-console.log("ayyyyy");
-
-
+  //
+  // console.log("ayy its ready");
+  //
+  // function Answer(params) {
+  //   if(params) this.data = params;
+  //   this.render();
+  // }
+  //
+  // Answer.prototype = {
+  //   render: function () {
+  //     this.view = "<li class='answer'><input type='hidden' name='id' value='" + (this.data.id || "") + "' /><input type='hidden' name='questionId' value='" + this.data.questionId + "' /><textarea name='content'>" + (this.data.content || "") + "</textarea>";
+  //       if(!this.data.id){
+  //         this.view += "<button type='button' class='create'>Create</button>";
+  //       }else{
+  //         this.view += "<button type='button' class='save'>Save</button><button type='button' class='delete'>Delete</button>";
+  //       }
+  //       this.view += "</li>";
+  //   }
+  // }
+  //
+  // function Question(params, answersHtml) {
+  //   if(params){
+  //     this.data = params;
+  //     this.answersHtml = answersHtml;
+  //     this.render();
+  //   }
+  // }
+  //
+  // Question.prototype = {
+  //   render: function(){
+  //     this.view = "<div class='question'><input type='hidden' name='id' value='" + (this.data.id || "") + "' /><input type='text' name='title' value='" + (this.data.title || "") + "' />";
+  //     if(!this.data.id){
+  //       this.view += "<button type='button' class='create'>Create</button>";
+  //     }else{
+  //       this.view += "<button type='button' class='save'>Save</button><button type='button' class='delete'>Delete</button>";
+  //       this.view += "<ul>" + this.answersHtml + "</ul>";
+  //     }
+  //     this.view += "</div>";
+  //   }
+  // }
+  //
+  // var questions;
+  // var answers;
+  // $.ajax({
+  //   url: "/questions",
+  //   method: "get"
+  // }).then(function(response){
+  //   questions = response;
+  //   questions.push({title: ""});
+  //   return $.ajax({
+  //     url: "/answers",
+  //     method: "get"
+  //   })
+  // }).then(function(response){
+  //   var answersHtml, answer, question;
+  //   var questionsHtml = "";
+  //   answers = response;
+  //   for(var l = 0; l < questions.length; l++){
+  //     question = questions[l];
+  //     answersHtml = "";
+  //     answers.push({questionId: question.id, content: ""});
+  //     for(var t = 0; t < answers.length; t++){
+  //       if(question.id == 0) break;
+  //       answer = answers[t];
+  //       if(answer.questionId != question.id) continue;
+  //       answersHtml += new Task(answer).view;
+  //     }
+  //     questionsHtml += new List(question, answersHtml).view;
+  //   }
+  //   $("main").html(questionsHtml);
+  //
+  //
+  //
+  // })
 
 })
