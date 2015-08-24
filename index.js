5a61be0b31f01e0be5154bf31aba2acf1582328c
Jeremy Koulish <jkoulish@gmail.com>
2015-08-24 11:53:29 -0400

    added seeds file, touched a couple other files elsewhere

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var app = express();
 
-// var path = require("path");
-// var bodyParser = require("body-parser");
-//
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use("/", express.static(path.join(__dirname + "/app/assets")));
+var path = require("path");
+var bodyParser = require("body-parser");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/", express.static(path.join(__dirname + "/app/assets")));
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use("/", express.static(path.join(__dirname + "/app/assets")));
 
 app.get("/", function (req, res) {
   res.send(sequelize)
