a840d63f3c419568496588486bf2dc571ba1fa44
Walter <Walter-0@users.noreply.github.com>
2015-08-27 10:31:07 -0400

    fixed port compatability with heroku in index.js

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ app.use("/", questionsController);
 app.use("/", answersController);
 
 
-app.listen(3000, function () {
-  console.log("listening on port 3000");
-});
+var port = process.env.PORT || 3000;
+app.listen(port);
+
